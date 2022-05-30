e08b74094e17fb01131843e3612c3c0f1c1fa9c6
Gabriel Lafrance <gl.gabriellafrance@gmail.com>
2022-05-29 21:49:26 -0400

    PResque tout marche

--- src/main.js
+++ src/main.js
@@ -8,3 +8,5 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+import "../node_modules/bootstrap/dist/js/bootstrap.js"
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,54 @@
 <template> 
   <main>
     <div class="container-fluid">
-      <button class="btn btn-secondary m-1" v-on:change="filteredTags" :v-model="search" v-for="tag in tags">{{tag}}</button>
+      <button class="btn btn-primary m-1" :class="{active:isActive === i}" @click="filteredTags(tag); toggle(i);"  v-for="(tag, i) in tags" :key="i">{{tag}}</button>
     </div>
     <!-- <Button :info="this.tags"/> -->
-    <div v-for="(site, index) in sites" :key="index" :id="index" class="container-fluid border p-3 m-3">
-      <h4 v-if="site.url"><a :href="site.url">{{site.nom}}</a></h4>
-      <h4 v-else>{{site.nom}}</h4>
-      <p>{{site.description}}</p>
-      <button class="btn btn-secondary m-1" :v-model="tags" v-for="but in site.tags">{{but}}</button><br>
-      <button @click.native="deleteSite(site.id)" class="btn btn-danger m-1">Delete</button>
+    <div v-if="current.length === 0">
+      <div v-for="(site, index) in sites" :key="index" :id="index" class="container-fluid border p-3 m-3">
+        <h4 class="m-1" v-if="site.url"><a :href="site.url">{{site.nom}}</a></h4>
+        <h4 v-else>{{site.nom}}</h4>
+        <p class="m-1">{{site.description}}</p>
+        <button class="btn btn-primary m-1" v-for="but in site.tags">{{but}}</button><br>
+        <button @click.native="deleteSite(site.id)" class="btn btn-danger m-1">Delete</button>
+      </div>
     </div>
+    <div v-else>
+      <div v-for="(site, index) in current" :key="index" :id="index"      class="container-fluid border p-3 m-3">
+        <h4 class="m-1" v-if="site.url"><a :href="site.url">{{site.nom}}</a></h4>
+        <h4 v-else>{{site.nom}}</h4>
+        <p class="m-1">{{site.description}}</p>
+        <button class="btn btn-primary m-1" v-for="but in site.tags">{{but}}</button><br>
+        <button @click.native="deleteSite(site.id)" class="btn btn-danger m-1">Delete</button>
+      </div>
+    </div>
+
+    <div class="modal" tabindex="-1" id="reg-modal" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
   </main>
 </template>
 
 
 <script>
+import { KeepAlive } from "vue"
 import Button from "../components/Buttons.vue"
   export default {
     components: {Button},
@@ -23,13 +56,14 @@ import Button from "../components/Buttons.vue"
       return {
         sites: [],
         tags: [],
+        current: [],
+        filteredSites: [],
         search: "",
-        currentState: false
+        isActive: null
       }
     }, 
      methods: {
       async deleteSite(id) {
-        console.log("Here")
        await fetch(`https://api---vuejs-default-rtdb.firebaseio.com/sites/${id}.json`, {
          method: "DELETE"
        })
@@ -46,9 +80,6 @@ import Button from "../components/Buttons.vue"
         }
         let reverse = [...arr].reverse()
         this.sites = reverse
-
-
-
       },
       async loadTags() {
         let result = await fetch(`https://api---vuejs-default-rtdb.firebaseio.com/sites.json`)
@@ -64,6 +95,25 @@ import Button from "../components/Buttons.vue"
       let unique = [...new Set(merge)]
       this.tags = unique
 
+      },
+        filteredTags(tag) {
+          this.current = this.sites
+          let items = this.current.filter(item => {
+          return item.tags.includes(tag)
+          })
+        this.current = items
+      },
+      toggle(i) {
+        if(!!this.isActive) {
+          this.isActive = null
+          this.current = []
+          console.log("Here")
+          console.log(i)
+        } else {
+          console.log("Also here")
+          this.isActive = i
+        }
+      
       }
     },
     async mounted() {
@@ -72,22 +122,15 @@ import Button from "../components/Buttons.vue"
     
     },
     computed: {
-      filteredsites() {
-        
-      },
-      filteredTags() {
-        
-        console.log("click")
-        return this.tags.filter((tag) => {
-          return tag.match(this.search)
-        })
-        
-      }
-        
-      }
+ 
     }
-   
+  }
 
 
   
 </script>
+
+
+<style>
+
+</style>
